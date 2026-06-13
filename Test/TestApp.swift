8c1feb81c9ada08599a7a00fbcc5561d58e731a5
Leo <luoguofeng0401@gmail.com>
2026-06-13 10:58:56 +0800

    first commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Guofeng Luo on 2026-06-13.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
